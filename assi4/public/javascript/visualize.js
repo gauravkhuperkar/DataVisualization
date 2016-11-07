91fdc6adac16baea3c8a6091ba76cf5ec4a9a776
Gaurav Khuperkar <gauravkh@thoughtworks.com>
2016-11-07 14:24:45 +0530

    Creating table visualization for n numbers and its oprotions

--- assi4/public/javascript/visualize.js
+++ assi4/public/javascript/visualize.js
@@ -1,5 +1,5 @@
 var numbers = [1,2,3,4,5,6,7,8,9,10];
-var header = ["Title", "numbers 1 to 10"];
+var header = ["Title", "n = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10"];
 
 var getData = function() {
 	var squares = numbers.map(function(each) {
@@ -7,35 +7,42 @@ var getData = function() {
 	});
 
 	var logs = numbers.map(function(each){
-		return Math.log10(numbers);
+		return Math.log10(each);
 	});
 
 	var roundOfLogs = logs.map(function(each){
-		return Math.round(numbers);
+		return Math.round(each);
 	});
 
-	var data = {}
-	data[numbers.join(" ")] = squares.join(" ");
-	data[numbers.join(" ")] = logs.join(" ");
-	data[numbers.join(" ")] = roundOfLogs.join(" ");
+	var data = []
+	data.push(["n square",squares.join(" ")]);
+	data.push(["log(n)",logs.join(" ")]);
+	data.push(["log(n) rounded",roundOfLogs.join(" ")]);
 
 	return data;
-}
+};
+
+var makeTableRow = function(data, row) {
+	for (var i = 0; i < data.length; i++) {
+		row.append("tr")
+			.selectAll("th")
+			.data(data[i])
+			.enter()
+			.append("th")
+			.text(function(d){return d})
+	};
+};
 
 var loadTable = function() {
 	var table =  d3.select(".continer").append("table");
-	var head = table.append("thead");
-	var body = table.append("tbody");
+	var tableHead = table.append("thead");
+	var tableBody = table.append("tbody");
 
-	// for header
-	head.append("tr")
-		.selectAll("th")
-		.data(header)
-		.enter()
-		.append("th")
-		.text(function(h){return h})
+	// for table head
+	makeTableRow([header],tableHead)
 
-}
+	// for table body
+	makeTableRow(getData(), tableBody);
+};
 
 window.onload = loadTable
-
