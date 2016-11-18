14cdfda3772908d9ce9be607d1bb3c5a1f4ea76a
Gaurav Khuperkar <gauravkh@thoughtworks.com>
2016-11-18 12:27:02 +0530

    Added circles in sine line and moved d3 min js file to common and deleted unnecessary code

--- assi7/public/javascript/visualize.js
+++ assi7/public/javascript/visualize.js
@@ -1,27 +1,32 @@
+var WIDTH = 700;
+var height = 600;
+
 var translate = function (x, y) {
   return 'translate(' + x + ',' + y + ')';
 };
+
+
 
 var loadChart = function() {
 	var data = [{x:0, y:5},{x:1, y:9},{x:2, y:7},{x:3, y:5},{x:4, y:3},{x:6, y:4},{x:7, y:2},{x:8, y:3},{x:9, y:2}];
 	
 	var xScale = d3.scaleLinear()
 		    .domain([0,10])
-		    .range([0, 800]);
+		    .range([0, WIDTH]);
 
 	var yScale = d3.scaleLinear()
 	    .domain([0,10])
-	    .range([510, 0]);
+	    .range([height, 0]);
 
 	var svg = d3.select('.continer').append('svg')
-			.attr('width', 800)
-			.attr('height', 600);
+			.attr('width', WIDTH)
+			.attr('height', height+100);
 
 	var xAxis = d3.axisBottom(xScale).ticks(10);
 	var yAxis = d3.axisLeft(yScale).ticks(10);
 
 	svg.append('g')
-			.attr('transform', translate(31,570))
+			.attr('transform', translate(31,height+60))
 			.call(xAxis);
 
 	svg.append('g')
@@ -38,20 +43,31 @@ var loadChart = function() {
 
 	var chartGroup = svg.append('g');
 
-	console.log(yScale(Math.sin(6)))
-	chartGroup
-			.append("path")
+	chartGroup.append("path")
 			.classed('numberLine', true)
 			.datum(data)
 			.attr("d", numberLine)
 			.attr('transform', translate(30, 62));
 
 	chartGroup.append("path")
-			.classed('numberLine', true)
-			// .datum(data)
-			.attr("d", sinValuesLine(data))
+			.classed('sinValuesLine', true)
+			.datum(data)
+			.attr("d", sinValuesLine)
 			.attr('transform', translate(30, 62));	
 
+// append Sin Circle
+	svg.selectAll('circle').data(data)
+		.enter()
+		.append('circle')
+		.attr("class","circles")
+		.attr('cx', function(d){return xScale(d.x)})
+		.attr('cy', function(d){return yScale(Math.sin(d.x)) - yScale(5)})
+		.attr('r', 5)
+		.attr('transform', translate(30, 62));
+
+
+
+// append line Circle
 	chartGroup.selectAll("circle")
 				.data(data)
 				.enter()
@@ -63,5 +79,7 @@ var loadChart = function() {
 				.attr('transform', translate(30, 62));
 
 };
+
+
 
 window.onload = loadChart;
