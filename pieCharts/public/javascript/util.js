9e479853ab39aaa981912deb67c999c7956f5e44
Gaurav Khuperkar <gauravkh@thoughtworks.com>
2016-11-21 23:23:40 +0530

    extract and written code in util js for piechart and importing it in load chart.

---
+++ pieCharts/public/javascript/util.js
@@ -0,0 +1,38 @@
+var util = {}
+util.appendSvg = function() {
+	console.log("......................")
+	return d3.select('.continer').append('svg')
+		    .attr("width", 800)
+		    .attr("height", 600)
+		    .append('g')
+		    .attr('transform', 'translate(' + 400 + ',' + 300 + ')');
+}
+
+util.appendArc = function() {
+	return d3.arc()
+	    	.outerRadius(300)
+	    	.innerRadius(0);
+}
+util.appendPie = function() {
+	return d3.pie()
+        	.value(function(d) { return d;})
+		    .sort(null)
+}
+
+util.appendSemiPie = function() {
+	return util.appendPie()
+        	.startAngle(0*(Math.PI / 180))
+        	.endAngle(180*(Math.PI / 180));
+}
+
+util.appendPath = function(svg,pie,data,arc) {
+	var color = d3.schemeCategory20;
+	return svg.selectAll('path')
+	        .data(pie(data))
+	        .enter()
+	        .append('path')
+	        .attr('d', arc)
+	        .attr('fill', function (d, i) {
+	            return color[i];
+	        });
+}
