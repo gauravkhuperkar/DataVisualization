000e0f552de281472180285a5c7e384827f04803
Gaurav Khuperkar <gauravkh@thoughtworks.com>
2016-11-28 15:42:05 +0530

    append area and fill it with light blue color. create css as well for area charts. Assignment 12 completed.

--- areaCharts/public/javascript/visualize.js
+++ areaCharts/public/javascript/visualize.js
@@ -3,46 +3,53 @@ var appendGroupAndTranslateBy = function(x, y, svg) {
 }
 
 var getSineValues = function(d) {
-	return (Math.sin(3*d)+1)/2;
+	return (3*Math.sin(d))+5;
 }
 
 var loadChart = function() {
 	var WIDTH = 800 ,HEIGHT = 600, MARGIN = 30;
-	var data = [0,1,2,3,4,5,6,7,8,9];
+	var data = [0,1,2,3,4,5,6,7,8,9,10];
 
 	var xScale = d3.scaleLinear()
 	    .domain([0,10])
 	    .range([0, 700]);
 
 		var yScale = d3.scaleLinear()
-	    .domain([0,1])
-	    .range([600, 0]);
-
-	var xAxisTicks =  d3.scaleLinear()
-	    .domain([0,1])
-	    .range([0, 700]);    
+	    .domain([0,10])
+	    .range([600, 0]); 
 	
 	var svg = d3.select('.continer').append('svg')
 			.attr('width', WIDTH)
 			.attr('height', HEIGHT+100);
 
-	var xAxis = d3.axisBottom(xAxisTicks).ticks(10);
+	var xAxis = d3.axisBottom(xScale).ticks(10);
 	var yAxis = d3.axisLeft(yScale).ticks(10);  
 
 	appendGroupAndTranslateBy(MARGIN,HEIGHT+MARGIN,svg).call(xAxis);
 	appendGroupAndTranslateBy(MARGIN,MARGIN,svg).call(yAxis);
 
-	var chartGroup = appendGroupAndTranslateBy(MARGIN,MARGIN,svg);
+	var chartGroup = appendGroupAndTranslateBy(MARGIN+1,MARGIN,svg);
 
 	var numberLine = d3.line()
 					.curve(d3.curveLinear)
 					.x(function(d) {return xScale(d)})
 					.y(function(d) {return yScale(getSineValues(d))})
 
+	var area = d3.area()
+				.x(function(d) {return xScale(d)})
+				.y0(function(d) {return yScale(getSineValues(d))})
+				.y1(HEIGHT)
+
+
 	chartGroup.append("path")
-		.classed("numberLine", true)
 		.datum(data)
-		.attr("d", numberLine)
+		.classed("area", true)
+		.attr("d", area)
+
+	chartGroup.append("path")
+		.datum(data)
+		.classed("numberLine", true)
+		.attr("d", numberLine)	
 
 	chartGroup.selectAll('circle').data(data)
 		.enter()
