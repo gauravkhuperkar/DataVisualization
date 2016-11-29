78b2afb87951d9d9d63e223e3f673fa47df96602
Gaurav Khuperkar <gauravkh@thoughtworks.com>
2016-11-29 10:31:12 +0530

    extracting code in smaller functions to make it flexible for further requirements.

--- areaCharts/public/javascript/visualize.js
+++ areaCharts/public/javascript/visualize.js
@@ -1,3 +1,4 @@
+
 var appendGroupAndTranslateBy = function(x, y, svg) {
 	return svg.append('g').attr('transform', 'translate(' + x + ',' + y + ')');
 }
@@ -6,58 +7,84 @@ var getSineValues = function(d) {
 	return (3*Math.sin(d))+5;
 }
 
+var generateLine = function() {
+	return d3.line()
+		.curve(d3.curveLinear)
+		.x(function(d) {return xScale()(d)})
+		.y(function(d) {return yScale()(getSineValues(d))})
+}
+
+var generateArea = function(height) {
+	return d3.area()
+		.x(function(d) {return xScale()(d)})
+		.y0(function(d) {return yScale()(getSineValues(d))})
+		.y1(height)
+}
+
+var appendPathWithArea = function(data, group, height) {
+	return group.append("path")
+		.datum(data)
+		.classed("area", true)
+		.attr("d", generateArea(height))
+}
+
+var appendPathWithLine = function(data, group) {
+	return group.append("path")
+		.datum(data)
+		.classed("numberLine", true)
+		.attr("d", generateLine())
+}
+
+var area = function(data, group) {
+	return group.selectAll('circle').data(data)
+		.enter()
+		.append('circle')
+		.attr("class","circles")
+		.attr('cx', function(d){return xScale()(d)})
+		.attr('cy', function(d){return yScale()(getSineValues(d))})
+		.attr('r', 5)
+}
+
+var xScale = function() {
+	return d3.scaleLinear()
+	    .domain([0,10])
+	    .range([0, 700]);
+}
+
+var yScale = function() {
+	return d3.scaleLinear()
+	    .domain([0,10])
+	    .range([600, 0]);
+}
+
+var appendSvg = function(width,height) {
+	return d3.select('.continer').append('svg')
+		.attr('width', width)
+		.attr('height', height+100);
+}
+
+// load chart ----------------------------------------------------------------------------------------------------
+
 var loadChart = function() {
 	var WIDTH = 800 ,HEIGHT = 600, MARGIN = 30;
 	var data = [0,1,2,3,4,5,6,7,8,9,10];
-
-	var xScale = d3.scaleLinear()
-	    .domain([0,10])
-	    .range([0, 700]);
-
-		var yScale = d3.scaleLinear()
-	    .domain([0,10])
-	    .range([600, 0]); 
-	
-	var svg = d3.select('.continer').append('svg')
-			.attr('width', WIDTH)
-			.attr('height', HEIGHT+100);
-
-	var xAxis = d3.axisBottom(xScale).ticks(10);
-	var yAxis = d3.axisLeft(yScale).ticks(10);  
+	var svg = appendSvg(WIDTH,HEIGHT);
+	var xAxis = d3.axisBottom(xScale()).ticks(10);
+	var yAxis = d3.axisLeft(yScale()).ticks(10);  
 
 	appendGroupAndTranslateBy(MARGIN,HEIGHT+MARGIN,svg).call(xAxis);
 	appendGroupAndTranslateBy(MARGIN,MARGIN,svg).call(yAxis);
 
-	var chartGroup = appendGroupAndTranslateBy(MARGIN+1,MARGIN,svg);
+	var group = appendGroupAndTranslateBy(MARGIN+1,MARGIN,svg);
+	generateLine();
+	generateArea();
 
-	var numberLine = d3.line()
-					.curve(d3.curveLinear)
-					.x(function(d) {return xScale(d)})
-					.y(function(d) {return yScale(getSineValues(d))})
-
-	var area = d3.area()
-				.x(function(d) {return xScale(d)})
-				.y0(function(d) {return yScale(getSineValues(d))})
-				.y1(HEIGHT)
-
-
-	chartGroup.append("path")
-		.datum(data)
-		.classed("area", true)
-		.attr("d", area)
-
-	chartGroup.append("path")
-		.datum(data)
-		.classed("numberLine", true)
-		.attr("d", numberLine)	
-
-	chartGroup.selectAll('circle').data(data)
-		.enter()
-		.append('circle')
-		.attr("class","circles")
-		.attr('cx', function(d){return xScale(d)})
-		.attr('cy', function(d){return yScale(getSineValues(d))})
-		.attr('r', 5)
+	appendPathWithArea(data, group, HEIGHT);
+	appendPathWithLine(data, group);
+	area(data, group);
 };
 
+//-----------------------------------------------------------------------------------------------------------------
+
 window.onload = loadChart
+
